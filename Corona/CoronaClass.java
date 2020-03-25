a60e5bc4412dbf1d31a1d7a1a7b329a02a361500
minh1308 <hvu@wustl.edu>
2020-03-25 15:41:30 -0500

    minh update

--- Corona/CoronaClass.java
+++ Corona/CoronaClass.java
@@ -7,7 +7,6 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 
-
 //import org.json.simple.JSONObject;
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -16,12 +15,13 @@ import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
 import org.json.JSONObject;
+
 public class CoronaClass {
 
 	private static HttpURLConnection connection;
-	
+
 	public static void main(String[] args) {
-		
+		// a
 		BufferedReader reader;
 		String line;
 		StringBuffer responseContent = new StringBuffer();
@@ -34,65 +34,56 @@ public class CoronaClass {
 			connection.setReadTimeout(5000);
 			connection.setRequestProperty("X-RapidAPI-Key", "7a7f939378mshbdfd83ae56d2d66p1b4c72jsndbf6cfd52b50");
 			int status = connection.getResponseCode();
-			
-			if(status > 299) {
+
+			if (status > 299) {
 				reader = new BufferedReader(new InputStreamReader(connection.getErrorStream()));
-				while((line = reader.readLine()) != null) {
+				while ((line = reader.readLine()) != null) {
 					responseContent.append(line);
 				}
 				reader.close();
-			}
-			else { //connection successful
-				
+			} else { // connection successful
+
 				reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-				while((line = reader.readLine()) != null) {
+				while ((line = reader.readLine()) != null) {
 					responseContent.append(line);
 				}
 				reader.close();
 			}
-			
+
 			totalCases = parseJson(responseContent.toString());
-			
-			
-		}
-		catch (MalformedURLException e) {
+
+		} catch (MalformedURLException e) {
 			e.printStackTrace();
-		}
-		catch (IOException e) {
+		} catch (IOException e) {
 			e.printStackTrace();
-		}
-		finally {
+		} finally {
 			connection.disconnect();
 		}
-		
-		
-////		
+
+		////
 		JFrame f = new JFrame("COVID-19 Tracker");
 		JLabel label = new JLabel();
 		label.setText(totalCases);
-		
-	    f.setSize(1000, 1000);
-	    f.setLocation(300,200);
-	    final JTextArea textArea = new JTextArea(100, 140);
-	    f.getContentPane().add(BorderLayout.CENTER, textArea);
-	    final JButton button = new JButton("Refresh");
-	    f.getContentPane().add(BorderLayout.SOUTH, button);
-	    button.addActionListener(new ActionListener() {
 
-	        @Override
-	        public void actionPerformed(ActionEvent e) {
-	            textArea.append("Update information\n");
+		f.setSize(1000, 1000);
+		f.setLocation(300, 200);
+		final JTextArea textArea = new JTextArea(100, 140);
+		f.getContentPane().add(BorderLayout.CENTER, textArea);
+		final JButton button = new JButton("Refresh");
+		f.getContentPane().add(BorderLayout.SOUTH, button);
+		button.addActionListener(new ActionListener() {
 
-	        }
-	    });
-	    f.add(label);
-	    f.setVisible(true);
-	    
-	    
-	    
-	    
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				textArea.append("Update information\n");
+
+			}
+		});
+		f.add(label);
+		f.setVisible(true);
 
 	}
+
 	public static String parseJson(String responseBody) {
 		JSONObject obj = new JSONObject(responseBody);
 		System.out.println("total cases:" + obj.getString("total_cases"));
