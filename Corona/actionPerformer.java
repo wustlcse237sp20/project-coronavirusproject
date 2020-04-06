cb10296a47a0ad8ea9e714e426f171e63025df9a
Ricardo Blohm <ricardoblohm@Ricardos-MacBook-Pro.local>
2020-04-05 20:04:09 -0400

    Display

--- Corona/actionPerformer.java
+++ Corona/actionPerformer.java
@@ -1,7 +1,9 @@
+
 import java.awt.BorderLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import javax.swing.*;
+import java.awt.*;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
@@ -11,14 +13,20 @@ public class actionPerformer implements ActionListener {
 	JFrame f = new JFrame("COVID-19 Tracker");
 	final JTextArea textArea = new JTextArea(100, 140);
 	final JButton button = new JButton("Refresh");
+	JComboBox combobox;
 	
 	public actionPerformer() {
+		combobox = new JComboBox(new Object[] {});
 		f.setSize(1000, 1000);
-		f.setLocation(300, 200);
+		f.setLocation(500, 500);
 		f.getContentPane().add(BorderLayout.CENTER, textArea);
 		f.getContentPane().add(BorderLayout.SOUTH, button);
 		f.setVisible(true);
 		button.addActionListener(this);
+		
+		//f.add(combobox);
+		
+		
 	}
 	
 	@Override
