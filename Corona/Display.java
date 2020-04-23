8d46a8527b6a8077c4afe513247305ed410e60bf
Zachary Serotte <zserotte@elon.edu>
2020-04-23 16:10:28 -0400

    Added development branch

--- Corona/Display.java
+++ Corona/Display.java
@@ -4,15 +4,18 @@ import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JTextArea;
-import javax.swing.SpringLayout;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import java.awt.Font;
 
 public class Display implements ActionListener {
 	JFrame frame;
 	private JButton btnRefresh;
 	private JTextArea textArea;
 	private JTextField textField;
+	private JTextField textField2;
 
 	/**
 	 * Create the application.
@@ -20,8 +23,11 @@ public class Display implements ActionListener {
 	public Display() {
 		frame = new JFrame();
 		btnRefresh = new JButton("Search");
+		btnRefresh.setBounds(425, 10, 85, 29);
 		textField = new JTextField();
-		textArea = new JTextArea(100, 140);
+		textField.setBounds(77, 10, 130, 26);
+		textArea = new JTextArea(13, 20);
+		textArea.setBounds(2, 2, 512, 157);
 		initialize();
 	}
 
@@ -29,24 +35,38 @@ public class Display implements ActionListener {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame.setBounds(100, 100, 450, 300);
+		frame.setBounds(100, 100, 516, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		SpringLayout springLayout = new SpringLayout();
-		frame.getContentPane().setLayout(springLayout);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, btnRefresh, 10, SpringLayout.NORTH, frame.getContentPane());
-		springLayout.putConstraint(SpringLayout.EAST, btnRefresh, -103, SpringLayout.EAST, frame.getContentPane());
+		frame.getContentPane().setLayout(null);
 		frame.getContentPane().add(btnRefresh);
-		
-		springLayout.putConstraint(SpringLayout.NORTH, textField, 0, SpringLayout.NORTH, btnRefresh);
-		springLayout.putConstraint(SpringLayout.EAST, textField, -7, SpringLayout.WEST, btnRefresh);
 		frame.getContentPane().add(textField);
 		textField.setColumns(10);
 		
 		textArea.setBackground(SystemColor.window);
-		springLayout.putConstraint(SpringLayout.NORTH, textArea, 10, SpringLayout.SOUTH, btnRefresh);
 		frame.getContentPane().add(textArea);
 		btnRefresh.addActionListener(this);
+		
+		JLabel lblCountry = new JLabel("Country");
+		lblCountry.setBounds(21, 15, 61, 16);
+		frame.getContentPane().add(lblCountry);
+		
+		JLabel province = new JLabel("Province");
+		province.setBounds(224, 15, 61, 16);
+		frame.getContentPane().add(province);
+		
+		textField2 = new JTextField();
+		textField2.setBounds(283, 10, 130, 26);
+		frame.getContentPane().add(textField2);
+		textField2.setColumns(10);
+		
+		JLabel lblIfYouWould = new JLabel("If you would like to display national cases only, leave Province field empty");
+		lblIfYouWould.setFont(new Font("Lucida Grande", Font.PLAIN, 9));
+		lblIfYouWould.setBounds(87, 38, 346, 16);
+		frame.getContentPane().add(lblIfYouWould);
+		
+		JScrollPane scroll = new JScrollPane(textArea,JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+		scroll.setBounds(0, 66, 516, 213);
+		frame.getContentPane().add(scroll);
 	}
 	
 	@Override
