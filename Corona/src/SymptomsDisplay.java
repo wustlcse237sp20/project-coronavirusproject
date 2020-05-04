c4492d2106711d9fb02baec046c5788e30218d0d
minh1308 <hvu@wustl.edu>
2020-05-04 00:52:08 -0500

    minh update

--- Corona/src/SymptomsDisplay.java
+++ Corona/src/SymptomsDisplay.java
@@ -272,8 +272,11 @@ public class SymptomsDisplay implements ActionListener {
 		return (Math.round(chanceDeath*100.0)/100.0);
 	}
 	
+	
+	
 	@Override
 	public void actionPerformed(ActionEvent e) {
+		textArea.setText("");
 		String action = e.getActionCommand();
 		if (action.equals("Home")) {
 			frame.dispose();
@@ -289,40 +292,53 @@ public class SymptomsDisplay implements ActionListener {
 				}
 			});
 		} else if (action.equals("Submit")) {
-			
-			String city = residenceTextField.getText().strip().toLowerCase();
-			int age = Integer.parseInt(ageTextField.getText().trim());
-			String country = countryTextField.getText().strip().toLowerCase(); 
+			if(residenceTextField.getText().isEmpty() || ageTextField.getText().isEmpty() || weightTextField.getText().isEmpty() || heightTextField.getText().isEmpty() || countryTextField.getText().isEmpty()) {
+				textArea.append("PLEASE FILL OUT ALL FIELDS");
+			}
+			try {
+				String city = residenceTextField.getText().strip().toLowerCase();
+				int age = Integer.parseInt(ageTextField.getText().trim());
+				String country = countryTextField.getText().strip().toLowerCase(); 
 
-			Vector<Boolean> symptoms = new Vector<Boolean>(8);
-			Vector<Boolean> conditions = new Vector<Boolean>(9); 
-			String[] infectedCities = {"new york city", "cook", "nassau", "suffolk", "westchester", "los angeles", "wayne", "bergen", "hudson", "philadelphia"};
-			String[] infectedCountries = {"usa", "italy", "spain", "germany", "china"};
+				Vector<Boolean> symptoms = new Vector<Boolean>(8);
+				Vector<Boolean> conditions = new Vector<Boolean>(9); 
+				String[] infectedCities = {"new york city", "cook", "nassau", "suffolk", "westchester", "los angeles", "wayne", "bergen", "hudson", "philadelphia"};
+				String[] infectedCountries = {"usa", "italy", "spain", "germany", "china"};
+				
+				symptoms.add(chckbxSoreThroat.isSelected());
+				symptoms.add(chckbxFever.isSelected());
+				symptoms.add(chckbxShortnessOfBreath.isSelected());
+				symptoms.add(chckbxMusclePain.isSelected());
+				symptoms.add(chckbxCough.isSelected());
+				symptoms.add(chckbxChills.isSelected());
+				symptoms.add(chckbxHeadache.isSelected());
+				symptoms.add(chckbxLossOfSmelltaste.isSelected());
 			
-			symptoms.add(chckbxSoreThroat.isSelected());
-			symptoms.add(chckbxFever.isSelected());
-			symptoms.add(chckbxShortnessOfBreath.isSelected());
-			symptoms.add(chckbxMusclePain.isSelected());
-			symptoms.add(chckbxCough.isSelected());
-			symptoms.add(chckbxChills.isSelected());
-			symptoms.add(chckbxHeadache.isSelected());
-			symptoms.add(chckbxLossOfSmelltaste.isSelected());
-		
-			conditions.add(chckbxKidneyDisease.isSelected());
-			conditions.add(chckbxLiverDisease.isSelected());
-			conditions.add(chckbxLungDisease.isSelected());
-			conditions.add(chckbxAsthma.isSelected());
-			conditions.add(chckbxNursingHome.isSelected());
-			conditions.add(chkbxHeart.isSelected());
-			conditions.add(chckbxImmunocompromised.isSelected());
-			conditions.add(chckbxObesity.isSelected());
-			conditions.add(chckbxDiabetes.isSelected());
+				conditions.add(chckbxKidneyDisease.isSelected());
+				conditions.add(chckbxLiverDisease.isSelected());
+				conditions.add(chckbxLungDisease.isSelected());
+				conditions.add(chckbxAsthma.isSelected());
+				conditions.add(chckbxNursingHome.isSelected());
+				conditions.add(chkbxHeart.isSelected());
+				conditions.add(chckbxImmunocompromised.isSelected());
+				conditions.add(chckbxObesity.isSelected());
+				conditions.add(chckbxDiabetes.isSelected());
+				
+				
+				riskScore(age, city, country, symptoms, infectedCities, infectedCountries, conditions);
+			}
+			catch (NumberFormatException event) {
+				textArea.setText("Please input a number");
+			}
 			
-			
-			riskScore(age, city, country, symptoms, infectedCities, infectedCountries, conditions);
 
 			
 		}
 		
 	}
 }
+
+//if(residenceTextField.getText().isEmpty() || ageTextField.getText().isEmpty() || weightTextField.getText().isEmpty() || heightTextField.getText().isEmpty() || countryTextField.getText().isEmpty()) {
+//	textArea.append("PLEASE FILL OUT ALL FIELDS");
+//	
+//}
